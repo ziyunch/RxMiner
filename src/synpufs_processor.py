5c7e48dba74ce9c6e7b9efb2f7a6e118cb990d78
ziyunch <ziyunch@gmail.com>
2019-01-24 02:46:49 -0500

    test

--- src/synpufs_processor.py
+++ src/synpufs_processor.py
@@ -6,6 +6,9 @@ from pyspark.sql import SparkSession
 from functools import reduce  # For Python 3.x
 from pyspark.sql import DataFrame
 
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.11:2.3.0 --conf spark.cassandra.connection.host=127.0.0.1 pyspark-shell'
+
+
 def unionAll(*dfs):
     return reduce(DataFrame.unionAll, dfs)
 
@@ -28,9 +31,19 @@ def import_sample():
 def link_synpufs():
     dr_rxevent.leftOuterJoin(df_bene1)
 
+def load_and_get_table_df(keys_space_name, table_name):
+    table_df = sqlContext.read\
+        .format("org.apache.spark.sql.cassandra")\
+        .options(table=table_name, keyspace=keys_space_name)\
+        .load()
+    return table_df
+
 import_sample()
+
 df.write\
     .format("org.apache.spark.sql.cassandra")\
     .mode('append')\
     .options(table="kv", keyspace="test")\
     .save()
+
+df.show(10)
