c2f3bcd08f86d478e62061c5ab9938607a5a526e
ziyunch <ziyunch@gmail.com>
2019-01-30 13:29:58 -0500

    test

--- src/db_import.py
+++ src/db_import.py
@@ -1,9 +1,7 @@
-import math, os
+import os
 import boto
-import requests
 import pandas as pd
 from sodapy import Socrata
-from filechunkio import FileChunkIO
 
 def import_soda(client_path, end_point, file_name):
     clinet = Socrate(client_path, rxminer_token)
@@ -26,48 +24,6 @@ def upload_pupd():
     # Uploads the given file using a managed uploader, which will split up large
     # files automatically and upload parts in parallel.
     s3.upload_file(filename, bucket_name, filename)
-
-def import_file():
-    # Unauthenticated client only works with public data sets. Note 'None'
-    # in place of application token, and no username or password:
-    client = Socrata("data.medicaid.gov", rxminer_token)
-    # Example authenticated client (needed for non-public datasets):
-    # client = Socrata(data.medicaid.gov,
-    #                  MyAppToken,
-    #                  userame="user@example.com",
-    #                  password="AFakePassword")
-
-    # First 2000 results, returned as JSON from API / converted to Python list of
-    # dictionaries by sodapy.
-    results = client.get("neai-csgh", limit=10000)
-    results_df = pd.DataFrame.from_records(results)
-    export_csv = results_df.to_csv(r'../test/sample/medicaid-sdud-2016.csv', index=None, header=True)
-    # read puf file
-    client = Socrata("data.cms.gov", rxminer_token)
-    results = client.get("haqy-eqp7", limit=10000)
-    results_df = pd.DataFrame.from_records(results)
-    export_csv = results_df.to_csv(r'../test/sample/medicare-puf-2016.csv', index=None, header=True)
-    #read medicare-part d puf file
-    results = client.get("ep4w-t37d", limit=10000)
-    results_df = pd.DataFrame.from_records(results)
-    export_csv = results_df.to_csv(r'../test/sample/medicare-partd-puf-2016.csv', index=None, header=True)
-    # read open payment data
-    client = Socrata("openpaymentsdata.cms.gov", rxminer_token)
-    results = client.get("a3k9-9uq3", limit=10000)
-    results_df = pd.DataFrame.from_records(results)
-    export_csv = results_df.to_csv(r'../test/sample/openpayment-gen-2016.csv', index=None, header=True)
-    # read wa sample
-    client = Socrata("data.wa.gov", rxminer_token)
-    results = client.get("t8zq-rh9q", limit=10000, clinicUse="y")
-    results_df = pd.DataFrame.from_records(results)
-    export_csv = results_df.to_csv(r'../test/sample/wa-pmp-2016q1.csv', index=None, header=True)
-    """
-    soda_api = "https://data.medicaid.gov/api/views/3v6v-qk5s/rows.csv?accessType=DOWNLOAD"
-    stream data into aws S3, doesn't work
-    s3_object = boto3.resource('s3').Object(bucket_name, object_key)
-    with requests.get(soda_api, stream=True) as r:
-        s3_object.put(Body=r.content)
-    """
 
 def main():
     # Connect to S3
