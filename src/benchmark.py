12a027a1c63377c60ef503d97bca6a7574c3b085
ziyunch <ziyunch@gmail.com>
2019-01-31 14:15:36 -0500

    use copy instead of pd.to_sql

--- src/benchmark.py
+++ src/benchmark.py
@@ -32,7 +32,7 @@ def df_to_postgres(df, table_name, mode):
     raw = engine.raw_connection()
     curs = raw.cursor()
     curs.execute("DROP TABLE " + table_name)
-    empty_table = pd.io.sql.get_schema(df, table_name, con = con)
+    empty_table = pd.io.sql.get_schema(df, table_name, con = engine)
     empty_table = empty_table.replace('"', '')
     curs.execute(empty_table)
     curs.copy_from(data, table_name, sep = ',')
