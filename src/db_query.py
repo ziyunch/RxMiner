d2e2047ef365c35296a3d5e0f31bdbcafb37abef
ziyunch <ziyunch@gmail.com>
2019-01-31 19:33:19 -0500

    test

--- src/db_query.py
+++ src/db_query.py
@@ -1,18 +1,16 @@
 import os
 import psycopg2
 
-def sum_by_state(cur):
+def sum_by_state():
     sql_query = """
         SELECT SUM(total_claim_count), practice_state
         FROM pupd_cleaned
         GROUP BY pupd_cleaned;
     """
     cur.execute(sql_query)
-    print("The number of parts: ", cur.rowcount)
-    rows = cur.fetchmany(size=10)
-    print(rows)
+    conn.commit()
 
-def merge_npi(cur):
+def merge_npi():
     sql_query = """
         SELECT
             pupd.npi,
@@ -25,9 +23,7 @@ def merge_npi(cur):
         LEFT JOIN npidata ON (npidata.npi = pupd.npi AND npidata.last_name = pupd.nppes_provider_last_org_name);
     """
     cur.execute(sql_query)
-    print("The number of parts: ", cur.rowcount)
-    rows = cur.fetchmany(size=10)
-    print(rows)
+    conn.commit()
 
 if __name__ == "__main__":
     user = os.getenv('POSTGRESQL_USER', 'default')
@@ -38,7 +34,7 @@ if __name__ == "__main__":
     conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=pswd)
     cur = conn.cursor()
     print("PostgreSQL connected")
-    merge_npi(cur)
-    sum_by_state(cur)
+    merge_npi()
+    sum_by_state()
     cur.close()
     conn.close()
