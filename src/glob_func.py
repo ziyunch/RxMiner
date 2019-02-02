34da55d3f8b4935a8c142a673c0d14331fe1be86
ziyunch <ziyunch@gmail.com>
2019-02-01 23:06:18 -0500

    more structured benchmark

--- src/glob_func.py
+++ src/glob_func.py
@@ -24,45 +24,32 @@ def df_to_sql(df, table_name, mode, new_table, psql, cur, engine):
     #curs = raw.cursor()
     if mode == 'replace':
         cur.execute("DROP TABLE " + table_name)
-        sql_query = """
-            COPY %s FROM STDIN WITH
-                CSV
-                HEADER;
-            """
-    elif new_table == 0:
-        sql_query = """
-            COPY %s FROM STDIN WITH
-                CSV
-                HEADER;
-            """
-    elif psql == 'psql':
-        sql_query = """
-            COPY temp FROM STDIN WITH
-                CSV
-                HEADER;
+    sql_query = """
+        COPY %s FROM STDIN WITH
+            CSV
+            HEADER;
         """
+    if (mode == 'replace' or new_table == 0):
+        sql_query2 = """
+            ALTER TABLE temp
+            RENAME TO %s;
+        """
+    elif psql == 'psql':
         sql_query2 = """
             INSERT INTO %s SELECT * FROM temp;
-            DROP TABLE temp;
         """
     else:
-        sql_query = """
-            COPY temp FROM STDIN WITH
-                CSV
-                HEADER;
-        """
         sql_query2 = """
             ALTER TABLE %s APPEND FROM temp;
+        """
+    sql_query3 = """
             DROP TABLE temp;
         """
-    empty_table = pd.io.sql.get_schema(df, table_name, con = engine)
+    empty_table = pd.io.sql.get_schema(df, temp, con = engine)
     empty_table = empty_table.replace('"', '')
     cur.execute(empty_table)
-    if (mode == 'replace' or new_table == 0):
-        cur.copy_expert(sql=sql_query % table_name, file=data)
-    else:
-        cur.copy_expert(sql=sql_query, file=data)
+    cur.copy_expert(sql=sql_query, file=data)
     cur.connection.commit()
-    if (mode == 'append' and new_table != 0):
-        cur.execute(sql_query2 % table_name)
-        cur.connection.commit()
+    cur.execute(sql_query2 % table_name)
+    cur.execute(sql_query3)
+    cur.connection.commit()
