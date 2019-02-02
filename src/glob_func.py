d5b8a220b2fc75f076ecf0394b988a97cdc7da70
ziyunch <ziyunch@gmail.com>
2019-02-01 23:19:38 -0500

    more structured benchmark

--- src/glob_func.py
+++ src/glob_func.py
@@ -16,6 +16,7 @@ def df_to_sql(df, table_name, mode, new_table, psql, cur, engine):
     Save DataFrame to .csv, read csv as sql table in memory and copy the table
      directly in batch to PostgreSQL or Redshift.
     """
+    df[:0].to_sql('temp', engine, if_exists = "replace")
     data = StringIO.StringIO()
     df.columns = cleanColumns(df.columns)
     df.to_csv(data, header=False, index=False)
@@ -24,6 +25,9 @@ def df_to_sql(df, table_name, mode, new_table, psql, cur, engine):
     #curs = raw.cursor()
     if mode == 'replace':
         cur.execute("DROP TABLE " + table_name)
+    empty_table = pd.io.sql.get_schema(df, 'temp', con = engine)
+    empty_table = empty_table.replace('"', '')
+    cur.execute(empty_table)
     sql_query = """
         COPY temp FROM STDIN WITH
             CSV
@@ -45,9 +49,6 @@ def df_to_sql(df, table_name, mode, new_table, psql, cur, engine):
     sql_query3 = """
             DROP TABLE temp;
         """
-    empty_table = pd.io.sql.get_schema(df, 'temp', con = engine)
-    empty_table = empty_table.replace('"', '')
-    cur.execute(empty_table)
     cur.copy_expert(sql=sql_query, file=data)
     cur.connection.commit()
     cur.execute(sql_query2 % table_name)
