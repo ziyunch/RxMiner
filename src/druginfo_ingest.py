6746a27b08c99628ce106005d16b9f4c83667ea0
ziyunch <ziyunch@gmail.com>
2019-02-06 16:42:57 -0500

    minor fix

--- src/druginfo_ingest.py
+++ src/druginfo_ingest.py
@@ -35,8 +35,11 @@ def read_drugndc(mode, new_table):
     df11 = df[['package_ndc', 'generic_name', 'product_ndc']]
     glob_func.df_to_redshift(df11, 'ndc11', mode, new_table, cur, engine, s3f)
     df9 = df[['product_ndc', 'generic_name', 'brand_name', 'labeler_name']]
-    df9 = rxgen_parse.rxgen_class(regex_df, df9, 'generic_name')
+    df9 = rxgen_parser.rxgen_class(regex_df, df9, 'generic_name')
     glob_func.df_to_redshift(df9, 'ndc9', mode, new_table, cur, engine, s3f)
+    genclass = df9[['generic_name', 'rxclass']]
+    genclass.drop_duplicates(subset ='generic_name', keep = 'first', inplace = True)
+    glob_func.df_to_redshift(genclass, 'genclass', mode, new_table, cur, engine, s3f)
     print(glob_func.time_stamp()+' Finish Reading NDC and save in table ndcdata')
 
 if __name__ == "__main__":
