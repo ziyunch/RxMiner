5ca3e9b5f39946fa313d721c6af86700ee1a1f43
ziyunch <ziyunch@gmail.com>
2019-02-06 22:41:58 -0500

    minor fix

--- src/medicare_ingest.py
+++ src/medicare_ingest.py
@@ -3,18 +3,18 @@ import glob_func
 import db_connect
 import rxgen_parser
 
-def collect_gen(df, df1):
-    df.append([df1['generic_name']])
-    df.drop_duplicates(keep = 'first', inplace = True)
-    return df
+def collect_gen(ds, ds1):
+    ds.append([ds1['generic_name']])
+    ds.drop_duplicates(inplace = True)
+    return ds
 
-def read_medicare(year, mode, new_table, gen_df):
+def read_medicare(year, mode, new_table, gen_ds):
     type_dir = 'pupd/medicare_pupd_'
     table_name = 'pupd'
     chunks = pd.read_csv(s3_path+type_dir+str(year)+'.csv', chunksize=chunk_size)
     for chunk in chunks:
         chunk["year"] = year
-        gen_df = collect_gen(gen_df, chunk)
+        gen_ds = collect_gen(gen_ds, chunk)
         glob_func.df_to_redshift(chunk, table_name, mode, new_table, cur, engine, s3f)
         new_table = False
         print(glob_func.time_stamp()+' Medicare data: reading in progress...')
@@ -33,10 +33,11 @@ if __name__ == "__main__":
     chunk_size = 200000
     url = 'https://druginfo.nlm.nih.gov/drugportal/jsp/drugportal/DrugNameGenericStems.jsp'
     regex_df = rxgen_parser.regex_file(url)
-    gen_df = pd.DataFrame()
-    gen_df = read_medicare(2013, 'append', True, gen_df)
+    gen_ds = pd.Series()
+    gen_ds = read_medicare(2013, 'append', True, gen_ds)
     for year in [2014,2015,2016]:
-        gen_df = read_medicare(year, 'append', False, gen_df)
+        gen_ds = read_medicare(year, 'append', False, gen_ds)
+    gen_df = gen_ds.to_frame()
     gen_df = rxgen_parser.rxgen_class(regex_df, gen_df, 'generic_name')
     glob_func.df_to_redshift(gen_df, 'pupd_genclass', 'append', True, cur, engine, s3f)
     db_connection.close_engine()
