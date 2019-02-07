1ae085859fb7eabd0446c6fbd9db137664651246
ziyunch <ziyunch@gmail.com>
2019-02-07 12:08:41 -0500

    test

--- src/medicare_ingest_test.py
+++ src/medicare_ingest_test.py
@@ -3,23 +3,23 @@ import glob_func
 import db_connect
 import rxgen_parser
 
-def collect_gen(ds, ds1):
-    ds.append(ds1.generic_name)
-    ds.drop_duplicates(inplace = True)
-    return ds
+def collect_gen(df, df1):
+    df = df.append(df1.generic_name)
+    df.drop_duplicates(inplace = True)
+    return df
 
-def read_medicare(year, mode, new_table, gen_ds):
+def read_medicare(year, mode, new_table, gen_df):
     type_dir = 'pupd/medicare_pupd_'
     table_name = 'pupd'
     chunks = pd.read_csv(s3_path+type_dir+str(year)+'.csv', chunksize=chunk_size)
     for chunk in chunks:
         chunk["year"] = year
-        gen_ds = collect_gen(gen_ds, chunk)
+        gen_df = collect_gen(gen_df, chunk)
         #glob_func.df_to_redshift(chunk, table_name, mode, new_table, cur, engine, s3f)
         new_table = False
         print(glob_func.time_stamp()+' Medicare data: reading in progress...')
     print(glob_func.time_stamp()+' Finish Reading Medicare data and save in table '+table_name)
-    return gen_ds
+    return gen_df
 
 if __name__ == "__main__":
     # Disable `SettingWithCopyWarning`
@@ -33,11 +33,11 @@ if __name__ == "__main__":
     chunk_size = 2000000
     url = 'https://druginfo.nlm.nih.gov/drugportal/jsp/drugportal/DrugNameGenericStems.jsp'
     regex_df = rxgen_parser.regex_file(url)
-    gen_ds = pd.Series()
-    gen_ds = read_medicare(2013, 'append', True, gen_ds)
+    gen_df = pd.Series()
+    gen_df = read_medicare(2013, 'append', True, gen_df)
     for year in [2014,2015,2016]:
-        gen_ds = read_medicare(year, 'append', False, gen_ds)
-    gen_df = gen_ds.to_frame('generic_name')
+        gen_df = read_medicare(year, 'append', False, gen_df)
+    gen_df = gen_df.to_frame()
     gen_df = rxgen_parser.rxgen_class(regex_df, gen_df, 'generic_name')
     glob_func.df_to_redshift(gen_df, 'pupd_genclass', 'append', True, cur, engine, s3f)
     db_connection.close_engine()
