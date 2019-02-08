714600a3f8a0bd100e98f1482ab60e8bc91721df
ziyunch <ziyunch@gmail.com>
2019-02-08 15:14:15 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -3,7 +3,7 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import explode
 
 spark = SparkSession.builder.getOrCreate()
-#df = spark.read.format('xml').options(rowTag='drug').load('/home/ubuntu/RxMiner/src/drugbank.xml')
-df_rxevent = spark.read.csv('s3n://rxminer/SynPUFs/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_*.csv', header=True)
-#df.printSchema()
-df_rxevent.show(10)
+df = spark.read.format('xml').options(rowTag='drug').load('/home/ubuntu/RxMiner/src/drugbank.xml')
+#df_rxevent = spark.read.csv('s3n://rxminer/SynPUFs/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_*.csv', header=True)
+df.printSchema()
+df.show(10)
