e6b67748389b60a043d436f0f36c44847b8d0a75
ziyunch <ziyunch@gmail.com>
2019-02-08 14:57:34 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -3,6 +3,6 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import explode
 
 spark = SparkSession.builder.getOrCreate()
-df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugbank/drugbank.xml')
+df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugbank/drugbank/drugbank.xml')
 df.printSchema()
 df.show(10)
