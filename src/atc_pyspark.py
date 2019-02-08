dd00bf659505e7efae8376e28dfb41625de0dd3d
ziyunch <ziyunch@gmail.com>
2019-02-08 16:01:12 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import explode
 
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugbank/drugbank.xml')
-df.withColumn('product', explode(df.products.product)).show
+df = df.withColumn('product', explode(df.products.product))
 #df_rxevent = spark.read.csv('s3n://rxminer/SynPUFs/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_*.csv', header=True)
 #df.printSchema()
-#df.show(10)
+df.show(10)
