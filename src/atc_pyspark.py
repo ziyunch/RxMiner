54bdb0a8659c4451450b9bd016d0fcf9892164e0
ziyunch <ziyunch@gmail.com>
2019-02-08 18:31:44 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -6,15 +6,15 @@ spark = SparkSession.builder.getOrCreate()
 df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugbank/drugbank.xml')
 df = df.select('name','products', 'atc-codes')
 # explode to get "long" format
-df = df.withColumn('product', F.explode(df.products.product))
-df = df.select('name', 'atc-codes', 'product.ndc-product-code')
-df = df.withColumn('exploded', F.explode(df.'atc-codes'.'atc-code'))
+#df = df.withColumn('product', F.explode(df.products.product))
+#df = df.select('name', col('atc-codes').alias("atc_codes"), 'product.ndc-product-code')
+#df = df.withColumn('exploded', F.explode(df.atc_codes.atc-code))
 # get the name and the name in separate columns
-df = df.withColumn('name', F.col('exploded').getItem(0))
-df = df.withColumn('value', F.col('exploded').getItem(1))
+#df = df.withColumn('name', F.col('exploded').getItem(0))
+#df = df.withColumn('value', F.col('exploded').getItem(1))
 # now pivot
 df.groupby('Id').pivot('name').agg(F.max('value')).na.fill(0)
 
 #df_rxevent = spark.read.csv('s3n://rxminer/SynPUFs/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_*.csv', header=True)
 df.printSchema()
-df.show(100)
+df.show(10)
