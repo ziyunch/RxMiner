2c8df4890f21406725c08bc5a9141aa65c742b52
ziyunch <ziyunch@gmail.com>
2019-02-08 17:16:58 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -1,12 +1,19 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import explode
+import pyspark.sql.functions as F
 
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugbank/drugbank.xml')
 df = df.select('name','products', 'atc-codes')
-df = df.withColumn('product', explode(df.products.product))
+# explode to get "long" format
+df = df.withColumn('product', F.explode(df.products.product))
 df = df.select('name', 'atc-codes', 'product.ndc-product-code')
+df = df.withColumn('exploded', F.explode(df.atc-codes.atc-code))
+# get the name and the name in separate columns
+df = df.withColumn('name', F.col('exploded').getItem(0))
+df = df.withColumn('value', F.col('exploded').getItem(1))
+# now pivot
+df.groupby('Id').pivot('name').agg(F.max('value')).na.fill(0)
 
 #df_rxevent = spark.read.csv('s3n://rxminer/SynPUFs/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_*.csv', header=True)
 df.printSchema()
