4fa058a783305d844fd4341f421f0190000b1ddf
ziyunch <ziyunch@gmail.com>
2019-02-08 14:40:24 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import explode
 
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugbank/drugbank.xml')
