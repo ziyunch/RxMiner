da3401ae4b84c1bbaf7d48fcc753a83a04e4f6e6
ziyunch <ziyunch@gmail.com>
2019-02-10 14:33:23 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -12,10 +12,10 @@ df = df.withColumn('product', F.explode(df.products.product))
 df = df.withColumnRenamed("atc-codes", "atccodes")
 df = df.withColumn('exploded', F.explode(df.atccodes['atc-code']))
 # get the name and the name in separate columns
-df = df.withColumn('name', F.col('exploded').getItem(0))
-df = df.withColumn('value', F.col('exploded').getItem(1))
+#df = df.withColumn('name', F.col('exploded').getItem(0))
+#df = df.withColumn('value', F.col('exploded').getItem(1))
 # now pivot
-df.groupby('Id').pivot('name').agg(F.max('value')).na.fill(0)
+#df.groupby('Id').pivot('name').agg(F.max('value')).na.fill(0)
 
 #df_rxevent = spark.read.csv('s3n://rxminer/SynPUFs/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_*.csv', header=True)
 df.printSchema()
