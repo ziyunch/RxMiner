b597fd16d6ec920d1f53f90e66634e3d22ebb199
ziyunch <ziyunch@gmail.com>
2019-02-10 13:55:15 -0500

    test

--- src/atc_pyspark.py
+++ src/atc_pyspark.py
@@ -9,8 +9,9 @@ df = spark.read.format('xml').options(rowTag='drug').load('s3n://rxminer/drugban
 df = df.select('name','products', 'atc-codes')
 # explode to get "long" format
 df = df.withColumn('product', F.explode(df.products.product))
-str_schema = "atccodes:struct<atccode:array<struct<string,array<struct<string,string>>>>>"
-df.select(F.col('atc-codes').cast(str_schema)).printSchema()
+df = df.withColumnRenamed("atc-codes", "atccodes")
+str_schema = "struct<atccode:array<struct<string,array<struct<string,string>>>>>"
+df.select(F.col('atccodes').cast(str_schema)).printSchema()
 df = df.withColumn('exploded', F.explode(df.atccodes.atccode))
 # get the name and the name in separate columns
 df = df.withColumn('name', F.col('exploded').getItem(0))
