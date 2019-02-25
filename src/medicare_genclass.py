0fc69fc95f579bb058fad53f9747c5f19cf5992f
ziyunch <ziyunch@gmail.com>
2019-02-25 02:58:22 -0500

    Use a seperate gen class table for medicare, see if I can speed it up. (previously 3 min for 200000 rows)

--- src/medicare_genclass.py
+++ src/medicare_genclass.py
@@ -3,17 +3,23 @@ import glob_func
 import db_connect
 import rxgen_parser
 
-def read_medicare(year, mode, new_table):
+def collect_gen(df, df1):
+    df = df.append(df1.generic_name)
+    df.drop_duplicates(inplace = True)
+    return df
+
+def read_medicare(year, mode, new_table, gen_df):
     type_dir = 'pupd/medicare_pupd_'
     table_name = 'pupd'
     chunks = pd.read_csv(s3_path+type_dir+str(year)+'.csv', chunksize=chunk_size)
     for chunk in chunks:
         chunk["year"] = year
-        chunk = rxgen_parser.rxgen_class(regex_df, chunk, 'generic_name')
-        glob_func.df_to_redshift(chunk, table_name, mode, new_table, cur, engine, s3f)
+        gen_df = collect_gen(gen_df, chunk)
+        #glob_func.df_to_redshift(chunk, table_name, mode, new_table, cur, engine, s3f)
         new_table = False
         print(glob_func.time_stamp()+' Medicare data: reading in progress...')
     print(glob_func.time_stamp()+' Finish Reading Medicare data and save in table '+table_name)
+    return gen_df
 
 if __name__ == "__main__":
     # Disable `SettingWithCopyWarning`
@@ -24,11 +30,15 @@ if __name__ == "__main__":
     s3f = db_connection.s3_fuse()
     s3_path = 's3n://rxminer/'
     new_table = 0
-    chunk_size = 200000
+    chunk_size = 2000000
     url = 'https://druginfo.nlm.nih.gov/drugportal/jsp/drugportal/DrugNameGenericStems.jsp'
     regex_df = rxgen_parser.regex_file(url)
-    read_medicare(2013, 'append', True)
+    gen_df = pd.Series()
+    gen_df = read_medicare(2013, 'append', True, gen_df)
     for year in [2014,2015,2016]:
-        read_medicare(year, 'append', False)
+        gen_df = read_medicare(year, 'append', False, gen_df)
+    gen_df = gen_df.to_frame()
+    gen_df = rxgen_parser.rxgen_class(regex_df, gen_df, 'generic_name')
+    glob_func.df_to_redshift(gen_df, 'pupd_genclass', 'append', True, cur, engine, s3f)
     db_connection.close_engine()
     db_connection.close_conn()
